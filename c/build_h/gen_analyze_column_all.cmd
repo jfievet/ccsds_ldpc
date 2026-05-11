f3085684ee5325165d24995f4a659db894047e84
jfievet <jfievet@protonmail.com>
2026-05-11 21:49:21 +0200

    Analyze min row distance for layered decoding

---
+++ c/build_h/gen_analyze_column_all.cmd
@@ -0,0 +1,14 @@
+gcc -O3 -Wall -Wextra -std=c11 -o analyze_H_column analyze_H_column.c
+
+analyze_H_column.exe H_1_2_1024.mat
+analyze_H_column.exe H_1_2_4096.mat
+analyze_H_column.exe H_1_2_16384.mat
+
+analyze_H_column.exe H_2_3_1024.mat
+analyze_H_column.exe H_2_3_4096.mat
+analyze_H_column.exe H_2_3_16384.mat
+
+analyze_H_column.exe H_4_5_1024.mat
+analyze_H_column.exe H_4_5_4096.mat
+analyze_H_column.exe H_4_5_16384.mat
+
